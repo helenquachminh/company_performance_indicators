1c2ea45114d5636ca9d19cae62e5ee6b236bfed3
helenquachminh <95054899+helenquachminh@users.noreply.github.com>
2021-11-28 00:02:00 +0000

    Update daily_ARPPU.sql

--- daily_ARPPU.sql
+++ daily_ARPPU.sql
@@ -1,7 +1,20 @@
+with daily_revenue as (
+  select
+    date(created_at) as dt,
+    round(sum(price), 2) as rev
+  from purchases
+  where refunded_at is null
+  group by 1
+),
+daily_players as (
+  select
+    date(created_at) as dt,
+    count(distinct user_id) as players
+  from gameplays
+  group by 1
+)
 select
-  date(created_at),
-  round(sum(price) / count(distinct user_id), 2) as arppu
-from purchases
-where refunded_at is null
-group by 1
-order by 1;
+  daily_revenue.dt as date,
+  daily_revenue.rev / daily_players.players as revenue per player
+from daily_revenue
+  join daily_players using (dt);
